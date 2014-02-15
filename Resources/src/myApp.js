8f1709d3818e94f2cc0a48d17d15d2633b5d3648
Robert Chang <cht8687@gmail.com>
2014-02-15 15:37:37 +1100

    format code

--- Resources/src/myApp.js
+++ Resources/src/myApp.js
@@ -24,140 +24,141 @@
  THE SOFTWARE.
  ****************************************************************************/
 var MyLayer = cc.Layer.extend({
-                              isMouseDown: false,
-                              helloImg: null,
-                              helloLabel: null,
-                              circle: null,
-                              sprite: null,
-                              river_Background: null,
-                              tap_sprite: null,
                               
-                              ctor: function () {
-                              this._super();
-                              cc.associateWithNative(this, cc.Layer);
-                              },
-                              
-                              init: function () {
-                              
-                              //////////////////////////////
-                              // 1. super init first
-                              this._super();
-                              
-                              if ('touches' in sys.capabilities) {
-                              
-                              this.setTouchMode(cc.TOUCH_ALL_AT_ONCE);
-                              this.setTouchEnabled(true);
-                              
-                              }
-                              
-                              //add background image (river)
-                              this.sprite = cc.Sprite.create("res/RiverBackground-static.png");
-                              this.sprite.setAnchorPoint(cc.p(0, 0));
-                              this.sprite.setPosition(cc.p(0, 0));
-                              this.addChild(this.sprite, 0);
-                              
-                              
-                              /////////////////////////////
-                              // 2. add a menu item with "X" image, which is clicked to quit the program
-                              //    you may modify it.
-                              // ask director the window size
-                              var size = cc.Director.getInstance().getWinSize();
-                              
-                              // add a "close" icon to exit the progress. it's an autorelease object
-                              
-                              /*
-                              var closeItem = cc.MenuItemImage.create(
-                                                                      "res/CloseNormal.png",
-                                                                      "res/CloseSelected.png",
-                                                                      function () {
-                                                                      cc.log("close button was clicked.");
-                                                                      }, this);
-                              closeItem.setAnchorPoint(cc.p(0.5, 0.5));
-                              
-                              var menu = cc.Menu.create(closeItem);
-                              menu.setPosition(cc.p(0, 0));
-                              this.addChild(menu, 1);
-                              closeItem.setPosition(cc.p(size.width - 20, 20));
-                              */
-                              
-                              /////////////////////////////
-                              // 3. add your codes below...
-                              // add a label shows "Hello World"
-                              // create and initialize a label
-                              this.helloLabel = cc.LabelTTF.create("Bath Duck", "Arial", 30);
-                              // position the label on the center of the screen
-                              this.helloLabel.setPosition(cc.p(size.width / 2, size.height - 100));
-                              // add the label as a child to this layer
-                              this.addChild(this.helloLabel, 5);
-                              
-                              // add "Helloworld" splash screen"
-                              this.sprite = cc.Sprite.create("res/ducksmall.png");
-                              this.sprite.setAnchorPoint(cc.p(0.5, 0.5));
-                              this.sprite.setPosition(cc.p(65,size.height/2));
-                              this.addChild(this.sprite, 0);
-                              this.tap_sprite = cc.Sprite.create("res/tap.png");
-                              this.tap_sprite.setAnchorPoint(cc.p(0.5, 0.5));
-                              this.tap_sprite.setPosition(cc.p(195, (size.height/2)-40));
-                              this.addChild(this.tap_sprite, 0);
-                              
-                              
-                              //var sprite_action = cc.Place.create(cc.p(0,300));
-                              //this.sprite.runAction(sprite_action);
-                              var bezier = [cc.p(0,100),cc.p(0,-100),cc.p(0,0)];
-                              var sprite_action = cc.BezierBy.create(5,bezier);
-                              //var sprite_fly_up = cc.MoveBy.create(2,cc.p(0,50));
-                              //var sprite_fly_down = cc.MoveBy.create(2,cc.p(0,-50));
-                              //var sequence_action = cc.Sequence.create(sprite_fly_up,sprite_fly_down);
-                              var sprite_action_2 = cc.RepeatForever.create(sprite_action);
-                              this.sprite.runAction(sprite_action_2);
-                              
-                              
-                              /*
-                               var sprite_action3 = cc.RotateTo.create(2,180);
-                               var repeate_action = cc.Repeat.create(sprite_action3,10);
-                               this.sprite.runAction(repeate_action);
-                               */
-                              
-                              /* var sprite_action3 = cc.SkewTo.create(2,2,2);
-                               this.sprite.runAction(sprite_action3);
-                               */
-                              
-                              return true;
-                              },
-                              
-                              
-                              onTouchesBegan: function (touches, event) {
-                              cc.log("Single touch has occured");
-                              var x = touches[0].getLocation().x;
-                              var y = touches[0].getLocation().y;
-                              
-                              var sprite_action = cc.MoveTo.create(2, cc.p(x, y));
-                              this.sprite.runAction(sprite_action);
-                              
-                              },
-                              
-                              onTouchesMoved: function (touches, event) {
-                              var x = touches[0].getLocation().x;
-                              var y = touches[0].getLocation().y;
-                              
-                              var sprite_action = cc.MoveTo.create(2, cc.p(x, y));
-                              this.sprite.runAction(sprite_action);
-                              
-                              
-                              }
-                              
-                              });
+        isMouseDown: false,
+        helloImg: null,
+        helloLabel: null,
+        circle: null,
+        sprite: null,
+        river_Background: null,
+        tap_sprite: null,
+        
+        ctor: function () {
+        this._super();
+        cc.associateWithNative(this, cc.Layer);
+        },
+        
+        init: function () {
+        
+        //////////////////////////////
+        // 1. super init first
+        this._super();
+        
+        if ('touches' in sys.capabilities) {
+        
+        this.setTouchMode(cc.TOUCH_ALL_AT_ONCE);
+        this.setTouchEnabled(true);
+        
+        }
+        
+        //add background image (river)
+        this.sprite = cc.Sprite.create("res/RiverBackground-static.png");
+        this.sprite.setAnchorPoint(cc.p(0, 0));
+        this.sprite.setPosition(cc.p(0, 0));
+        this.addChild(this.sprite, 0);
+        
+        
+        /////////////////////////////
+        // 2. add a menu item with "X" image, which is clicked to quit the program
+        //    you may modify it.
+        // ask director the window size
+        var size = cc.Director.getInstance().getWinSize();
+        
+        // add a "close" icon to exit the progress. it's an autorelease object
+        
+        /*
+        var closeItem = cc.MenuItemImage.create(
+                                                "res/CloseNormal.png",
+                                                "res/CloseSelected.png",
+                                                function () {
+                                                cc.log("close button was clicked.");
+                                                }, this);
+        closeItem.setAnchorPoint(cc.p(0.5, 0.5));
+        
+        var menu = cc.Menu.create(closeItem);
+        menu.setPosition(cc.p(0, 0));
+        this.addChild(menu, 1);
+        closeItem.setPosition(cc.p(size.width - 20, 20));
+        */
+        
+        /////////////////////////////
+        // 3. add your codes below...
+        // add a label shows "Hello World"
+        // create and initialize a label
+        this.helloLabel = cc.LabelTTF.create("Bath Duck", "Arial", 30);
+        // position the label on the center of the screen
+        this.helloLabel.setPosition(cc.p(size.width / 2, size.height - 100));
+        // add the label as a child to this layer
+        this.addChild(this.helloLabel, 5);
+        
+        // add "Helloworld" splash screen"
+        this.sprite = cc.Sprite.create("res/ducksmall.png");
+        this.sprite.setAnchorPoint(cc.p(0.5, 0.5));
+        this.sprite.setPosition(cc.p(65,size.height/2));
+        this.addChild(this.sprite, 0);
+        this.tap_sprite = cc.Sprite.create("res/tap.png");
+        this.tap_sprite.setAnchorPoint(cc.p(0.5, 0.5));
+        this.tap_sprite.setPosition(cc.p(195, (size.height/2)-40));
+        this.addChild(this.tap_sprite, 0);
+        
+        
+        //var sprite_action = cc.Place.create(cc.p(0,300));
+        //this.sprite.runAction(sprite_action);
+        var bezier = [cc.p(0,100),cc.p(0,-100),cc.p(0,0)];
+        var sprite_action = cc.BezierBy.create(5,bezier);
+        //var sprite_fly_up = cc.MoveBy.create(2,cc.p(0,50));
+        //var sprite_fly_down = cc.MoveBy.create(2,cc.p(0,-50));
+        //var sequence_action = cc.Sequence.create(sprite_fly_up,sprite_fly_down);
+        var sprite_action_2 = cc.RepeatForever.create(sprite_action);
+        this.sprite.runAction(sprite_action_2);
+        
+        
+        /*
+         var sprite_action3 = cc.RotateTo.create(2,180);
+         var repeate_action = cc.Repeat.create(sprite_action3,10);
+         this.sprite.runAction(repeate_action);
+         */
+        
+        /* var sprite_action3 = cc.SkewTo.create(2,2,2);
+         this.sprite.runAction(sprite_action3);
+         */
+        
+        return true;
+        },
+        
+        
+        onTouchesBegan: function (touches, event) {
+        cc.log("Single touch has occured");
+        var x = touches[0].getLocation().x;
+        var y = touches[0].getLocation().y;
+        
+        var sprite_action = cc.MoveTo.create(2, cc.p(x, y));
+        this.sprite.runAction(sprite_action);
+        
+        },
+        
+        onTouchesMoved: function (touches, event) {
+        var x = touches[0].getLocation().x;
+        var y = touches[0].getLocation().y;
+        
+        var sprite_action = cc.MoveTo.create(2, cc.p(x, y));
+        this.sprite.runAction(sprite_action);
+        
+        
+        }
+        
+});
 
 var MyScene = cc.Scene.extend({
-                              ctor: function () {
-                              this._super();
-                              cc.associateWithNative(this, cc.Scene);
-                              },
-                              
-                              onEnter: function () {
-                              this._super();
-                              var layer = new MyLayer();
-                              this.addChild(layer);
-                              layer.init();
-                              }
-                              });
+   ctor: function () {
+   this._super();
+   cc.associateWithNative(this, cc.Scene);
+   },
+   
+   onEnter: function () {
+   this._super();
+   var layer = new MyLayer();
+   this.addChild(layer);
+   layer.init();
+   }
+});
