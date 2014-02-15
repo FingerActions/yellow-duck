8a2089862e71db9978cb852b9dce680e533cbe2a
Robert Chang <cht8687@gmail.com>
2014-02-15 20:59:43 +1100

    added walls

--- Resources/src/playscene.js
+++ Resources/src/playscene.js
@@ -28,6 +28,8 @@
 
 var INITIALIZED_PLAYSCENE=false;
 
+var SCREENSIZE = null;
+
 var PlayLayer = cc.Layer.extend({
                               
         isMouseDown: false,
@@ -39,6 +41,8 @@ var PlayLayer = cc.Layer.extend({
         tap_sprite: null,
 		riverSprite: null,
         game_status:null,
+        wall_up:null,
+        wall_down:null,
         
         ctor: function () {
         this._super();
@@ -63,14 +67,14 @@ var PlayLayer = cc.Layer.extend({
         this.sprite.setAnchorPoint(cc.p(0, 0));
         this.sprite.setPosition(cc.p(0, 0));
         this.addChild(this.sprite, 0);
-        
+    
         
         /////////////////////////////
         // 2. add a menu item with "X" image, which is clicked to quit the program
         //    you may modify it.
         // ask director the window size
         var size = cc.Director.getInstance().getWinSize();
-        
+        SCREENSIZE = size;
         // add a "close" icon to exit the progress. it's an autorelease object
         
         /*
@@ -106,6 +110,8 @@ var PlayLayer = cc.Layer.extend({
         this.sprite.setAnchorPoint(cc.p(0.5, 0.5));
         this.sprite.setPosition(cc.p(65,size.height/2));
         this.addChild(this.sprite, 0);
+                                
+        this.createWall();
         
         /*
          var sprite_action3 = cc.RotateTo.create(2,180);
@@ -149,18 +155,32 @@ var PlayLayer = cc.Layer.extend({
          var sprite_action_2 = cc.RepeatForever.create(action_fall);
          
          this.sprite.runAction(sprite_action_2);
-         
-         
+        
          },
                                 
+         createWall: function(){
+            
+             this.wall_up = cc.Sprite.create("res/wall-up.png");
+             this.wall_up.setAnchorPoint(cc.p(0.5, 0.5));
+             this.wall_up.setPosition(cc.p(350,SCREENSIZE.height-50));
+             this.addChild(this.wall_up, 0);
+         
+             this.wall_down = cc.Sprite.create("res/wall-down.png");
+             this.wall_down.setAnchorPoint(cc.p(0.5, 0.5));
+             this.wall_down.setPosition(cc.p(350,50));
+             this.addChild(this.wall_down, 0);
+              
+             var scroll_up = cc.MoveBy.create(2,cc.p(-200,0));
+             var scroll_down = cc.MoveBy.create(2,cc.p(-200,0));
+            
                                 
-        PlayScene: function(){
-         
-                              
-         
-                              
-        }
-    
+             var sprite_action = cc.RepeatForever.create(scroll_up);
+             var sprite_action2 = cc.RepeatForever.create(scroll_down);
+             //var sprite_action2 = cc.MoveBy.create(-10, 0);
+             this.wall_up.runAction(sprite_action);
+             this.wall_down.runAction(sprite_action2);
+             //this.wall_down.runAction(sprite_action2);
+         },
         
 });
 
