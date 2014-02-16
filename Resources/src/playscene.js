49e785595c7f8e2fbc880edbcd1bd1b4aff1e37c
maochenyan <maochenyan@maochenyan.com>
2014-02-16 11:04:39 +1100

    refactored and added spawning waves

--- Resources/src/playscene.js
+++ Resources/src/playscene.js
@@ -1,218 +1,119 @@
-/****************************************************************************
- Copyright (c) 2010-2012 cocos2d-x.org
- Copyright (c) 2008-2010 Ricardo Quesada
- Copyright (c) 2011      Zynga Inc.
- 
- http://www.cocos2d-x.org
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- ****************************************************************************/
-
-
-
-var INITIALIZED_PLAYSCENE=false;
-
-var SCREENSIZE = null;
+var INITIALIZED_PLAYSCENE = false;
 
 var PlayLayer = cc.Layer.extend({
-                              
-        isMouseDown: false,
-        helloImg: null,
-        helloLabel: null,
-        circle: null,
-        sprite: null,
-        river_Background: null,
-        tap_sprite: null,
-		riverSprite: null,
-        game_status:null,
-        wall_up:null,
-        wall_down:null,
-        
-        ctor: function () {
+    _duck: null,
+    _background: null,
+    _river: null,
+    _wave: null,
+    _wallTop: null,
+    _wallBottom: null,
+    _screenSize: null,
+    _timer: null,
+    _duckVelocity: null,
+
+    ctor: function () {
         this._super();
         cc.associateWithNative(this, cc.Layer);
-        },
-        
-        init: function () {
-        
-        //////////////////////////////
-        // 1. super init first
+    },
+
+    init: function () {
+        //super init first
         this._super();
-        this.timer = 0;
+
+        //update()
+        this._timer = 0;
+        this.scheduleUpdate();
+
+        //touch
         if ('touches' in sys.capabilities) {
-        
-        this.setTouchMode(cc.TOUCH_ALL_AT_ONCE);
-        this.setTouchEnabled(true);
-        
+            this.setTouchMode(cc.TOUCH_ALL_AT_ONCE);
+            this.setTouchEnabled(true);
+        }
+
+        //add background image (river)
+        this._river = cc.Sprite.create("res/RiverBackground-static.png");
+        this._river.setAnchorPoint(cc.p(0, 0));
+        this._river.setPosition(cc.p(0, 0));
+        this.addChild(this._river);
+
+        //screen size
+        this._screenSize = cc.Director.getInstance().getWinSize();
+
+        this._duck = cc.Sprite.create("res/ducksmall.png");
+        this._duck.setAnchorPoint(cc.p(0.5, 0.5));
+        this._duck.setPosition(cc.p(65, this._screenSize.height / 2));
+        this.addChild(this._duck);
+        this._duckVelocity = 0;
+
+        //wave
+        this._wave = [];
+        return true;
+    },
+
+    spawnWave: function(){
+        this._wave[this._wave.length] = cc.Sprite.create("res/wave.png");
+
+        var waveSpawnPositionY = Math.floor(Math.random() * this._screenSize.height);
+        this._wave[this._wave.length - 1].setPosition(cc.p(400, waveSpawnPositionY));
+        this.addChild(this._wave[this._wave.length - 1]);
+
+        var flow = cc.MoveBy.create(10, cc.p(-500, -600));
+        this._wave[this._wave.length - 1].runAction(flow);
+    },
+
+    onTouchesBegan: function (touches, event){
+        this._duckVelocity = 10;
+    },
+
+    update: function(delta){
+        this._timer += delta;
+        if(this._timer > 1){
+            this.createWall();
+            this.spawnWave();
+            this._timer = 0;
         }
         
-        //add background image (river)
-        this.sprite = cc.Sprite.create("res/RiverBackground-static.png");
-        this.sprite.setAnchorPoint(cc.p(0, 0));
-        this.sprite.setPosition(cc.p(0, 0));
-        this.addChild(this.sprite, 0);
-    
+        var duckPrePosition = this._duck.getPosition();
+        this._duck.setPosition(cc.p(duckPrePosition.x, duckPrePosition.y + this._duckVelocity));
+        this._duckVelocity -= 0.5;
+    },
+
+    createWall: function(){
+        this._wallTop = cc.Sprite.create("res/wall-up.png");
+        this._wallTop.setAnchorPoint(cc.p(0.5, 0.5));
+        this._wallTop.setPosition(cc.p(350, this._screenSize.height - 50));
+        this.addChild(this._wallTop, 0);
+
+        this._wallBottom = cc.Sprite.create("res/wall-down.png");
+        this._wallBottom.setAnchorPoint(cc.p(0.5, 0.5));
+        this._wallBottom.setPosition(cc.p(350,50));
+        this.addChild(this._wallBottom, 0);
+
+        var scroll_up = cc.MoveBy.create(2,cc.p(-200,0));
+        var scroll_down = cc.MoveBy.create(2,cc.p(-200,0));
+
+        var sprite_action = cc.RepeatForever.create(scroll_up);
+        var sprite_action2 = cc.RepeatForever.create(scroll_down);
         
-        /////////////////////////////
-        // 2. add a menu item with "X" image, which is clicked to quit the program
-        //    you may modify it.
-        // ask director the window size
-        var size = cc.Director.getInstance().getWinSize();
-        SCREENSIZE = size;
-        // add a "close" icon to exit the progress. it's an autorelease object
-        
-        /*
-        var closeItem = cc.MenuItemImage.create(
-                                                "res/CloseNormal.png",
-                                                "res/CloseSelected.png",
-                                                function () {
-                                                cc.log("close button was clicked.");
-                                                }, this);
-        closeItem.setAnchorPoint(cc.p(0.5, 0.5));
-        
-        var menu = cc.Menu.create(closeItem);
-        menu.setPosition(cc.p(0, 0));
-        this.addChild(menu, 1);
-        closeItem.setPosition(cc.p(size.width - 20, 20));
-        */
-        
-        /////////////////////////////
-        // 3. add your codes below...
-        // add a label shows "Hello World"
-        // create and initialize a label
-                 
-		this.riverSprite = cc.Sprite.create("res/river.png");
-        this.riverSprite.setAnchorPoint(cc.p(0.5, 0.5));
-        this.riverSprite.setPosition(cc.p(350,size.height/2));
-        this.addChild(this.riverSprite, 0);
-		
-		var sprite_action = cc.MoveTo.create(10, cc.p(-100, size.height/2));
-		this.riverSprite.runAction(sprite_action);
-		
-        // add "Helloworld" splash screen"
-        this.sprite = cc.Sprite.create("res/ducksmall.png");
-        this.sprite.setAnchorPoint(cc.p(0.5, 0.5));
-        this.sprite.setPosition(cc.p(65,size.height/2));
-        this.addChild(this.sprite, 0);
-		
-        this.scheduleUpdate();
-        
-        /*
-         var sprite_action3 = cc.RotateTo.create(2,180);
-         var repeate_action = cc.Repeat.create(sprite_action3,10);
-         this.sprite.runAction(repeate_action);
-         */
-        
-        /* var sprite_action3 = cc.SkewTo.create(2,2,2);
-         this.sprite.runAction(sprite_action3);
-         */
-        
-        return true;
-        },
-        
-        
-        onTouchesBegan: function (touches, event) {
-                                
-                                
-            this.sprite.stopAllActions();
-                              
-            cc.log("Jump");
-            var jump = cc.JumpBy.create(1, cc.p(0, 0), 100, 1);
-			this.sprite.runAction(jump);
-        
-        },
-        
-         onTouchesEnded:function(touches,event){
-        
-         //this.sprite.stopAllActions();
-         
-         this.Duck_fall();
-         
-         
-         },
-         
-         Duck_fall:function(){
-        
-         
-         var action_fall = cc.MoveBy.create(1,cc.p(0,-200));
-         
-         var sprite_action_2 = cc.RepeatForever.create(action_fall);
-         
-         this.sprite.runAction(sprite_action_2);
-        
-         },
-        timer:null,
-         update:function(delta){
-		 this.timer += delta;
-			 if(this.timer > 1){
-			 
-				this.createWall();
-				this.timer = 0;
-			}
-         },
-                                
-         createWall: function(){
-            
-             this.wall_up = cc.Sprite.create("res/wall-up.png");
-             this.wall_up.setAnchorPoint(cc.p(0.5, 0.5));
-             this.wall_up.setPosition(cc.p(350,SCREENSIZE.height-50));
-             this.addChild(this.wall_up, 0);
-         
-             this.wall_down = cc.Sprite.create("res/wall-down.png");
-             this.wall_down.setAnchorPoint(cc.p(0.5, 0.5));
-             this.wall_down.setPosition(cc.p(350,50));
-             this.addChild(this.wall_down, 0);
-              
-             var scroll_up = cc.MoveBy.create(2,cc.p(-200,0));
-             var scroll_down = cc.MoveBy.create(2,cc.p(-200,0));
-            
-                                
-             var sprite_action = cc.RepeatForever.create(scroll_up);
-             var sprite_action2 = cc.RepeatForever.create(scroll_down);
-             //var sprite_action2 = cc.MoveBy.create(-10, 0);
-             this.wall_up.runAction(sprite_action);
-             this.wall_down.runAction(sprite_action2);
-             //this.wall_down.runAction(sprite_action2);
-                             
-         },
-        
+        this._wallTop.runAction(sprite_action);
+        this._wallBottom.runAction(sprite_action2);
+                     
+    }
 });
 
 var PlayScene = cc.Scene.extend({
-                              
-   ctor: function () {
-   this._super();
-   cc.associateWithNative(this, cc.Scene);
-                              
-   },
-   
-   onEnter: function () {
-    
-    if(INITIALIZED_PLAYSCENE==false)
-    {
-       INITIALIZED_PLAYSCENE = true;
-       this._super();
-       var layer = new PlayLayer();
-       this.addChild(layer);
-       layer.init();
+    ctor: function () {
+        this._super();
+        cc.associateWithNative(this, cc.Scene);                       
+    },
+
+    onEnter: function () {
+        if(INITIALIZED_PLAYSCENE==false){
+            INITIALIZED_PLAYSCENE = true;
+            this._super();
+            var layer = new PlayLayer();
+            this.addChild(layer);
+            layer.init();
+        }
     }
-                                
-   }
-                              
 });
