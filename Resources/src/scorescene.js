fb5993acbed1252c87b16f81ba347e7e068398e7
maochenyan <maochenyan@maochenyan.com>
2014-03-01 15:51:41 +1100

    finished socialnetwork url

--- Resources/src/scorescene.js
+++ Resources/src/scorescene.js
@@ -1,4 +1,3 @@
-
 //////////////////////////////////////////////////////////////////////
 //TIANMAO GAMES PROPRIETARY INFORMATION
 //
@@ -16,22 +15,23 @@
 
 var ScoreLayer = cc.Layer.extend({
 
-    _diebackground:null,
-    _timer:null,
-    _scorebanner:null,
-    _bestscore_label:null,
-    _currentscore_label:null,
-    _bestscore:null,
-    _currentscore:null,
-    _gameover:null,
-    _tapcontinue:null,
+    _diebackground: null,
+    _timer: null,
+    _scorebanner: null,
+    _bestscore_label: null,
+    _currentscore_label: null,
+    _bestscore: null,
+    _currentscore: null,
+    _gameover: null,
+    _tapcontinue: null,
+    _highScore: null,
 
-    ctor: function () {
+    ctor: function() {
         this._super();
         cc.associateWithNative(this, cc.Layer);
     },
 
-    init: function () {
+    init: function() {
 
         //////////////////////////////
         // 1. super init first
@@ -48,9 +48,9 @@ var ScoreLayer = cc.Layer.extend({
 
         //add background image (die)
         this._diebackground = cc.Sprite.create("res/img/background/die_scene.png");
-        this._diebackground.setAnchorPoint(cc.p(0,0));
-        this._diebackground.setPosition(cc.p(0,0));
-        this.addChild(this._diebackground,2);
+        this._diebackground.setAnchorPoint(cc.p(0, 0));
+        this._diebackground.setPosition(cc.p(0, 0));
+        this.addChild(this._diebackground, 2);
         this._diebackground.setVisible(true);
 
         //screen size
@@ -63,15 +63,16 @@ var ScoreLayer = cc.Layer.extend({
         this.size = cc.Director.getInstance().getWinSize();
 
         //create score banner
-        this._scorebanner = cc.LayerColor.create(cc.c4b(0,0,0,0),this.size.width-60,this.size.height-150);
-        this._scorebanner.setPosition(cc.p(30,this.size.height/10));
-        this.addChild(this._scorebanner,3);
-        var fadein = cc.FadeTo.create(1.0,150);
+        this._scorebanner = cc.LayerColor.create(cc.c4b(0, 0, 0, 0), this.size.width - 60, this.size.height - 150);
+        this._scorebanner.setPosition(cc.p(30, this.size.height / 10));
+        this.addChild(this._scorebanner, 3);
+        var fadein = cc.FadeTo.create(1.0, 150);
         this._scorebanner.runAction(fadein);
 
+        this._highScore = sys.localStorage.getItem('highScore')
         //add score on banner
         // create and initialize a label
-        this._bestscore = cc.LabelTTF.create(sys.localStorage.getItem('highScore'), "Marker Felt", 25);
+        this._bestscore = cc.LabelTTF.create(this._highScore, "Marker Felt", 25);
         // position the label on the center of the screen
         this._bestscore.setPosition(cc.p(this.size.width / 2 + 50, this.size.height - 200));
         // add the label as a child to this layer
@@ -89,7 +90,7 @@ var ScoreLayer = cc.Layer.extend({
         // create and initialize a label
         this._gameover = cc.LabelTTF.create("GAME OVER", "Marker Felt", 36);
         // position the label on the center of the screen
-        this._gameover.setPosition(cc.p(this.size.width / 2 , this.size.height - 130));
+        this._gameover.setPosition(cc.p(this.size.width / 2, this.size.height - 130));
         // add the label as a child to this layer
         this.addChild(this._gameover, 5);
         this._bestscore_label = cc.LabelTTF.create("Best", "Marker Felt", 25);
@@ -113,7 +114,7 @@ var ScoreLayer = cc.Layer.extend({
 
         var fadein_tap = cc.FadeIn.create(1.0);
         var fadeout_tap = cc.FadeOut.create(1.0);
-        var sequence = cc.RepeatForever.create(cc.Sequence.create(fadein_tap,fadeout_tap));
+        var sequence = cc.RepeatForever.create(cc.Sequence.create(fadein_tap, fadeout_tap));
 
         this._tapcontinue.runAction(sequence);
 
@@ -121,28 +122,43 @@ var ScoreLayer = cc.Layer.extend({
         var twitterButton = cc.MenuItemImage.create('res/twitter.png', 'res/twitter.png', this.tweet, this);
         twitterButton.setPosition(cc.p(-50, 0));
         twitterButton.setScale(0.4);
-        
-        var socialMenu = cc.Menu.create(twitterButton);
+
+        var facebookButton = cc.MenuItemImage.create('res/facebook.png', 'res/facebook.png', this.share, this);
+        facebookButton.setPosition(cc.p(50, 0));
+        facebookButton.setScale(0.4);
+
+        var socialMenu = cc.Menu.create(twitterButton, facebookButton);
         socialMenu.setPosition(cc.p(this.size.width / 2, this.size.height - 360));
         this.addChild(socialMenu, 5);
 
         return true;
     },
 
-    tweet: function(){
-        cc.log(cc.Application.getInstance());
-        cc.Application.getInstance().openURL("http://www.google.com");
-        //https://twitter.com/intent/tweet?
+    tweet: function() {
+        // todo: change to download url
+        cc.Application.getInstance().openURL('https://twitter.com/intent/tweet?text=I got ' + this._highScore + ' in Bath Duck! Download at... to challenge me!');
     },
 
-    onTouchesBegan: function (touches, event) {
+    share: function() {
+        // todo: change to download url
+        var app_id = '145634995501895';
+        var display = 'popup';
+        var name = 'Bath Duck';
+        var caption = 'I got ' + this._highScore + ' in Bath Duck!';
+        var description = 'Download at... to challenge me!';
+        var link = 'https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fdialogs%2F%20';
+        var redirect_uri = 'https://developers.facebook.com/tools/explorer';
+        cc.Application.getInstance().openURL('https://www.facebook.com/dialog/feed?app_id=' + app_id + '&display=' + display + '&name=' + name + '&caption=' + caption + '&description=' + description + '&link=' + link + '&redirect_uri=' + redirect_uri);
+    },
+
+    onTouchesBegan: function(touches, event) {
 
         cc.log("Single touch has occured");
         this.IntroScene();
 
     },
 
-    IntroScene: function(){
+    IntroScene: function() {
 
         var scene = cc.Scene.create();
         var layer = new MyScene();
@@ -155,12 +171,12 @@ var ScoreLayer = cc.Layer.extend({
 
 var ScoreScene = cc.Scene.extend({
 
-    ctor: function () {
+    ctor: function() {
         this._super();
         cc.associateWithNative(this, cc.Scene);
     },
 
-    onEnter: function () {
+    onEnter: function() {
         this._super();
         audioEngin = cc.AudioEngine.getInstance();
         var layer = new ScoreLayer();
