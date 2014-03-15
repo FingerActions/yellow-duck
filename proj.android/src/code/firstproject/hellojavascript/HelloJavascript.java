5e38d99f29832d1f8b6f867214134591b2868e40
steve mao <steve.mao@healthinteract.com.au>
2014-03-15 14:24:11 +1100

    starting android

--- proj.android/src/code/firstproject/hellojavascript/HelloJavascript.java
+++ proj.android/src/code/firstproject/hellojavascript/HelloJavascript.java
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ****************************************************************************/
-package org.cocos2dx.hellojavascript;
+package code.firstproject.hellojavascript;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
 
