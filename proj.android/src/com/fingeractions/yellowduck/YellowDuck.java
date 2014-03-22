25960ca9f7adf710cec80499d2feed8c7cdef743
steve mao <steve.mao@healthinteract.com.au>
2014-03-22 23:35:35 +1100

    login google!!

--- proj.android/src/com/fingeractions/yellowduck/YellowDuck.java
+++ proj.android/src/com/fingeractions/yellowduck/YellowDuck.java
@@ -25,16 +25,63 @@ package com.fingeractions.yellowduck;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
 import com.fingeractions.libFingerActions.FingerActions;
+import com.google.example.games.basegameutils.GameHelper;
+import com.google.example.games.basegameutils.GameHelper.GameHelperListener;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class YellowDuck extends Cocos2dxActivity{
 	
-	protected void onCreate(Bundle savedInstanceState){
-		super.onCreate(savedInstanceState);
+//	protected void onCreate(Bundle savedInstanceState){
+//		super.onCreate(savedInstanceState);
+//		
+//		FingerActions.initLibrary(this);
+//		FingerActions.addAdMob("portrait");
+//	}
+	
+	GameHelper mHelper;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    // create game helper with all APIs (Games, Plus, AppState):
+	    mHelper = new GameHelper(this, GameHelper.CLIENT_ALL);
+
+	    super.onCreate(savedInstanceState);
 		
 		FingerActions.initLibrary(this);
 		FingerActions.addAdMob("portrait");
+
+	    GameHelperListener listener = new GameHelper.GameHelperListener() {
+	        @Override
+	        public void onSignInSucceeded() {
+	            // handle sign-in succeess
+	        }
+	        @Override
+	        public void onSignInFailed() {
+	            // handle sign-in failure (e.g. show Sign In button)
+	        }
+
+	    };
+	    mHelper.setup(listener);
+	}
+	
+	@Override
+	protected void onStart() {
+	    super.onStart();
+	    mHelper.onStart(this);
+	}
+
+	@Override
+	protected void onStop() {
+	    super.onStop();
+	    mHelper.onStop();
+	}
+
+	@Override
+	protected void onActivityResult(int request, int response, Intent data) {
+	    super.onActivityResult(request, response, data);
+	    mHelper.onActivityResult(request, response, data);
 	}
 	
     static {
