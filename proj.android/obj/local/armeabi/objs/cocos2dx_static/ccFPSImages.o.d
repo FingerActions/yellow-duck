07e37bed2f7863b58c81758eea8c6b60f3b2f1aa
steve mao <steve.mao@healthinteract.com.au>
2014-04-09 21:39:08 +1000

    fixed icons

--- proj.android/obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o: \
- /cocos2dx/cocos2d-x-2.2.2/projects/YellowDuck/proj.android/../../../cocos2dx/ccFPSImages.c \
- /cocos2dx/cocos2d-x-2.2.2/projects/YellowDuck/proj.android/../../../cocos2dx/ccFPSImages.h
-
-/cocos2dx/cocos2d-x-2.2.2/projects/YellowDuck/proj.android/../../../cocos2dx/ccFPSImages.h:
--- proj.android/obj/local/armeabi/objs/cocos2dx_static/kazmath/src/neon_matrix_impl.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/kazmath/src/neon_matrix_impl.o: \
- /cocos2dx/cocos2d-x-2.2.2/projects/YellowDuck/proj.android/../../../cocos2dx/kazmath/src/neon_matrix_impl.c \
- /cocos2dx/cocos2d-x-2.2.2/projects/YellowDuck/proj.android/../../../cocos2dx/kazmath/include/kazmath/neon_matrix_impl.h
-
-/cocos2dx/cocos2d-x-2.2.2/projects/YellowDuck/proj.android/../../../cocos2dx/kazmath/include/kazmath/neon_matrix_impl.h:
